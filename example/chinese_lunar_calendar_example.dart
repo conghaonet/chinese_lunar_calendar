38020742bbc4fa561989cb4e9660acae3c90cac4
hao.cong <hao.cong@app2m.com>
2025-02-25 16:04:24 +0800

    在analysis_options.yaml中定义page_width: 120

--- example/chinese_lunar_calendar_example.dart
+++ example/chinese_lunar_calendar_example.dart
@@ -2,7 +2,7 @@ import 'package:chinese_lunar_calendar/chinese_lunar_calendar.dart';
 
 void main() {
   final lunarCalendar = LunarCalendar.from(
-    utcDateTime: DateTime(2024, 2, 6, 22, 30).toUtc(),
+    utcDateTime: DateTime(2025, 2, 25, 15, 30).toUtc(),
   );
 
   print('中国标准时间：${lunarCalendar.cst}');
